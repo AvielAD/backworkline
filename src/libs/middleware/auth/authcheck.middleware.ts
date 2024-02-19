63426583ca7cb232171511fc3c2bf787ea7b7a20
Aviel Aldama <aviel.aldama@gmail.com>
2024-02-18 22:22:52 -0600

    middleare auth

---
+++ src/libs/middleware/auth/authcheck.middleware.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+
+export const VerifyToken = async(req: Request, res: Response, next: NextFunction)=>{
+    try {
+        const token =  req.headers.authorization?.split(" ")[1]
+        if(!token){
+           return res.status(401).json([])
+        }
+        //efectuar llamado desde axios
+        const response = await fetch(`${process.env.AUTH_API}/api/user`,{
+            headers:{
+                "Authorization": `Bearer ${token}`
+            }
+        })
+        
+        if(response.status == 200){
+            next()
+        }
+        else {
+           return res.status(401).json([])
+        }
+    } catch (error) {
+        return res.status(401).json([])
+    }
+}
--- src/routes/tickets.route.ts
+++ src/routes/tickets.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import * as CtrlTickets from '../controllers/ticket.controller'
-
+import * as Auth from '../libs/middleware/auth/authcheck.middleware'
 const router = Router()
 
-router.get('/tickets', CtrlTickets.GetTickets)
-router.get('/tickets/:uuidSearch', CtrlTickets.GetTicketById)
-router.post('/tickets', CtrlTickets.IniciarTicket)
-router.put('/tickets/:uuidSearch', CtrlTickets.CerrarTicket)
+router.get('/tickets',[Auth.VerifyToken], CtrlTickets.GetTickets)
+router.get('/tickets/:uuidSearch',[Auth.VerifyToken], CtrlTickets.GetTicketById)
+router.post('/tickets', [Auth.VerifyToken], CtrlTickets.IniciarTicket)
+router.put('/tickets/:uuidSearch', [Auth.VerifyToken], CtrlTickets.CerrarTicket)
 
 export default router
